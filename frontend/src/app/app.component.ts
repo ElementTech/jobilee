17b593a51ec34a1eab2899b25a5df1e29f9c7cd9
Amitai Getzler <amitai.getzler@explorium.ai>
2023-01-12 20:15:06 +0200

    :pencil2: fix
    modified:   src/app/app.component.html; modified:   src/app/app.component.ts

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {MenuItem} from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +8,10 @@ import {MenuItem} from 'primeng/api';
 export class AppComponent {
   title = 'Jobilee';
 
-  items: MenuItem[];
   model: any[] = [];
-
+  constructor(){
+    
+  }
 
   ngOnInit() {
     this.model = [
